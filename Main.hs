c1733f7cd402c8a96ffbc2c4895770ee8825dd4b
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-01-16 19:30:06 +0100

    Modify reading from file to accept string as state name

--- Main.hs
+++ Main.hs
@@ -1,18 +1,28 @@
 import System.Environment
 
-getEdges :: String -> [(Char,Char)]
-getEdges [] = []
-getEdges (_:_:state:_:word:_:rest) = (state,word) : getEdges rest
+split :: String -> Char -> [String]
+split [] _ = [""]
+split (x:xs) regex 
+             | x == regex  = "" : rest
+             | otherwise = (x : head rest) : tail rest
+              where rest = split xs regex
 
-getFromLine :: String -> (Char,[(Char,Char)])
-getFromLine (state:rest) = (state,(getEdges rest))
+getTransitionsFromBracket :: String -> (String, String)
+getTransitionsFromBracket bracket = (state, symbol)
+                        where splittedBracket = split bracket ','
+                              state = tail $ head splittedBracket
+                              symbol = init $ last splittedBracket
 
-getList :: [String] -> [(Char,[(Char,Char)])]
-getList = map getFromLine
+getStateDescription :: String -> (String,[(String,String)])
+getStateDescription line = (head splittedLine, map (getTransitionsFromBracket) (tail splittedLine))
+                        where splittedLine = split line ' '
+
+getAutomatonDescription :: [String] -> [(String,[(String,String)])]
+getAutomatonDescription line = map (getStateDescription) line
 
 main :: IO ()
 main = do
   (fileName : _) <- getArgs
   fileContent <- readFile fileName
   let fileLines = (lines fileContent)
-  print (getList fileLines)
+  print (getAutomatonDescription fileLines)
