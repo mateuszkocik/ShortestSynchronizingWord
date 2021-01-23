fa8d31b0b7e8dc24b83dfdc01784cdb1d86f55c2
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-01-23 20:22:36 +0100

    Add instances into Automaton.hs

--- Automaton.hs
+++ Automaton.hs
@@ -4,7 +4,8 @@ module Automaton
     Transition(..),
     State(..),
     StateID,
-    Automaton(..)
+    Automaton(..),
+    getTransition
 ) where
 
 type Symbol = String
@@ -12,16 +13,37 @@ type Symbol = String
 type StateID = Int
 
 data Transition = Transition {symbol :: Symbol,
-                              toState :: StateID
-                              }deriving (Show)
+                              toState :: StateID}
+
+instance Show Transition where
+    show x = "(" ++ symbol x ++ "," ++ show (toState x) ++ ")"
+
+
 
 data State = State {stateId :: StateID,
-                    transitions :: [Transition]
-                    }deriving (Show)
+                    stateSize :: Int,
+                    transitions :: [Transition]}
 
+instance Show State where
+    show x = "Id = " ++ show (stateId x) ++ " Size = " ++ show (stateSize x) ++ "   " ++ show (transitions x) ++ "\n"
+
+instance Eq State where
+    (==) x y = stateId x == stateId y
+
+instance Ord State where
+    (State s1 _ _) `compare` (State s2 _ _) = s1 `compare` s2
+
+getTransition :: State -> Symbol -> Maybe (Int)
+getTransition s a = getTransition' (transitions s) a
+                  where
+                      getTransition' [] _ = Nothing
+                      getTransition' (x:xs) a = if (symbol x == a) then Just $ toState x
+                                                else getTransition' xs a
 
 data Automaton = Automaton {alphabet :: [Symbol],
-                            states :: [State],
-                            size :: Int
-                            }deriving (Show)
+                            automatonSize :: Int,
+                            states :: [State]}
+
+instance Show Automaton where
+    show x = "Alphabet: " ++ show (alphabet x) ++ "\nSize: " ++ show (automatonSize x) ++ "\n"  ++ show (states x) 
 
