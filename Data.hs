62cb6151536a7da4cda6ca203be6838db19a55b6
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-01-23 20:23:18 +0100

    Add Tree and few functions

--- Data.hs
+++ Data.hs
@@ -1,13 +1,19 @@
 module Data
 (
+    catMaybes,
     Queue(..),
     push,
     pop,
     Bin(..),
     convertInt,
-    convertBin
+    convertBin,
+    Tree(..),
+    buildTreeFromSortedList,
+    getPositions
 ) where
 
+catMaybes :: [Maybe a] -> [a]
+catMaybes ls = [x | Just x <- ls]
 
 data Queue a = Empty | Queue [a] [a]
 
@@ -23,7 +29,7 @@ pop (Queue (value:frontRest) back) = Just (value, reducedQueue)
                                                       else Queue frontRest back
 
 -- No Digits Binary
-data Bin = NDB | Zero Bin | One Bin 
+data Bin = NDB | Zero Bin | One Bin
 
 instance Show Bin where
     show NDB = ""
@@ -32,15 +38,29 @@ instance Show Bin where
 
 
 --convert functions for Bin in reversed order digits e.g. 1011 = 13
-
-convertInt :: Bin -> Integer
+convertInt :: Bin -> Int
 convertInt = decode (1,0)
             where decode (_,v) NDB = v
                   decode (pos,v) (Zero rest) = decode (pos*2,v) rest
                   decode (pos,v) (One rest) = decode (pos*2,v+pos) rest
 
-convertBin :: Integer -> Bin
+convertBin :: Int -> Bin
 convertBin x = encode NDB x
             where encode b 0 = b
                   encode b int = if int `mod` 2 == 1 then (One $ encode b (int `div` 2))
                                  else (Zero $ (encode b (int `div` 2)))
+
+getPositions :: Bin -> [Int]
+getPositions bin = getPositions' bin 0 []
+                where getPositions' NDB _ acc = acc
+                      getPositions' (One rest) v acc = getPositions' rest (v+1) (v:acc)
+                      getPositions' (Zero rest) v acc = getPositions' rest (v+1) acc
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving Show
+
+buildTreeFromSortedList :: (Ord a) => [a] -> Tree a
+buildTreeFromSortedList [] = EmptyTree
+buildTreeFromSortedList (x:xs) = Node x (buildTreeFromSortedList half1) (buildTreeFromSortedList half2)
+                                where (half1,half2) = splitAt (length xs `div` 2) xs
+
+
